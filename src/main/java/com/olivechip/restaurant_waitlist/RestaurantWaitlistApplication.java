a2504b6ad7e7b5b2ea725be32a021d6a9b6c127d
Oliver <oliver@MSI>
2025-02-20 17:18:14 -0800

    initial commit

---
+++ src/main/java/com/olivechip/restaurant_waitlist/RestaurantWaitlistApplication.java
@@ -0,0 +1,13 @@
+package com.olivechip.restaurant_waitlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantWaitlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantWaitlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olivechip/restaurant_waitlist/RestaurantWaitlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olivechip.restaurant_waitlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantWaitlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
