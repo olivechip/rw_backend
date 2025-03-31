1d8c556f9c6f5e78bb1829241bb0a4435ea77787
Oliver <oliver@MSI>
2025-03-30 21:45:14 -0700

    removed unused repo method

--- src/main/java/com/olivechip/restaurant_waitlist/repository/WaitlistEntryRepository.java
+++ src/main/java/com/olivechip/restaurant_waitlist/repository/WaitlistEntryRepository.java
@@ -1,7 +1,6 @@
 package com.olivechip.restaurant_waitlist.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +10,4 @@ import com.olivechip.restaurant_waitlist.entity.WaitlistEntry;
 @Repository
 public interface WaitlistEntryRepository extends JpaRepository<WaitlistEntry, Integer> {
     List<WaitlistEntry> findWaitlistEntriesByRestaurantId(Integer restaurantId);
-    Optional<WaitlistEntry> findWaitlistEntryByGuestId(Integer guestId);
 }
